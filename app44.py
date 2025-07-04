0360761e2764b944f4c1284eb98b95eaf5ff8835
Oceanwaves742 <oceanbach10@gmail.com>
2025-07-04 15:06:38 +0100

    Update app44.py

--- app44.py
+++ app44.py
@@ -34,8 +34,7 @@ if 'all_laps_df' not in st.session_state:
 # Fetch live data function
 @st.cache_data(ttl=25)  # small cache to avoid hammering source during quick reruns
 def fetch_live_data():
-    #url = "https://results.alphatiming.co.uk/api/v1/bpec/live/current"
-    url = "https://results.alphatiming.co.uk/api/v1/adventuresportswarwick/live/current"
+    url = "https://results.alphatiming.co.uk/api/v1/bpec/live/current"
 
     response = requests.get(url, verify=False)
     response.raise_for_status()
